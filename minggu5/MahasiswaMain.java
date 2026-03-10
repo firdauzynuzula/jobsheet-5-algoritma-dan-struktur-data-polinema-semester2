7bbb7c0e44296df785529c83e161e92e599a02f7
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-03-10 11:56:44 +0700

    menambahkan file tugas praktikum berupa Mahasiswa.java , MahasiswaMain.java, dan Nilai.java

---
+++ minggu5/MahasiswaMain.java
@@ -0,0 +1,12 @@
+package minggu5;
+
+public class MahasiswaMain {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+
+        mhs.nama = "budi";
+        mhs.nim = "254107000982";
+        mhs.tahunMasuk = 2025;
+        mhs.tampilInfo();
+    }
+}
---
+++ minggu5/Nilai.java
@@ -0,0 +1,16 @@
+package minggu5;
+
+public class Nilai {
+    int nilaiUts = 0;
+    int nilaiUas = 0;
+
+    int HitungNilaiTertinggi(int x , int y){
+        int Tertinggi = 0;
+
+        if (nilaiUas < x) {
+            return Tertinggi;
+        }else{
+            return nilaiUas;
+        }
+    }
+}
